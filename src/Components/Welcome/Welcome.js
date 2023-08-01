918466200d200f0a959c6abf4275a0a286be7d0f
edm1001 <edmer_franciz@yahoo.com>
2023-08-01 16:12:49 -0500

    initial files

---
+++ src/Components/Welcome/Welcome.js
@@ -0,0 +1,14 @@
+// import Button from 'react-bootstrap';
+
+import { Button } from "react-bootstrap";
+
+
+const Welcome =  () => {
+    return (
+        <>
+        hello world
+        <Button />
+        </>
+    )
+}
+export default Welcome;
