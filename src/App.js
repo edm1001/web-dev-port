def00bfa59c01a040f3b548efd51b6efa1f69c9e
edm1001 <edmer_franciz@yahoo.com>
2023-08-02 17:28:48 -0500

    make project comp

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Welcome from './Components/Welcome/Welcome.js';
 import About from './Components/About/About.js';
-import Services from './Components/Services/Services';
+import Services from './Components/Services/Services'
+import Projects from './Components/Projects/Projects';
 function App() {
   return (
     <>
     <Welcome/>
     <About/>
     <Services/>
+    <Projects/>
     </>
       );
 }
--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,6 +1,24 @@
+import { Row, Col, Card, Button } from "react-bootstrap";
+import Wordle from '../../Assets/port/wordle.gif'
 const Projects = () => {
     return (
         <section className="portfolio">
+            <h3> My Projects</h3>
+            <Row>
+                <Col lg={4} md={6} sm={12}>
+            <Card>
+            <Card.Header>
+                <h3>Wordle</h3>
+            </Card.Header>
+            <Card.Img
+            src={Wordle} alt='jo' />
+            <Card.Body>
+            
+                <Button>Hello</Button>
+            </Card.Body>
+            </Card> 
+                </Col>
+            </Row>
 
         </section>
     )
