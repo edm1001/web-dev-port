b4bfcb72b3eef295875a108c99402de788f475dc
edm1001 <edmer_franciz@yahoo.com>
2023-08-18 11:56:20 -0500

    add reviews

--- src/Components/Reviews/Reviews.js
+++ src/Components/Reviews/Reviews.js
@@ -5,13 +5,99 @@ import {BsFillPersonFill} from 'react-icons/bs'
 
 const Reviews =() => {
     return (
-        <section>
-            <Card>
-            <i><BsFillPersonFill/></i>
-            <Card.Body>
+        <section className='reviews'>
+            <Row className='reviews-row'>
 
+                <Col lg={3} md={4} sm={6}>
+            <Card>
+            <i className=''><BsFillPersonFill size={35} /></i>
+            <Card.Body>
+             <p>
+                Hi Edmer, <br/>
+            Your Regex tutorial on email validation is clean and readable! You were able to explain the purpose and the proper usage of this complex feature! 
+            You were able to provide a clear explanation of the underlying regex topics.  Great job including detailed sections that corresponded to important components in the starter code that make up the regex features. You were able to embed code snippets for each underlying example you used! 
+
+            You created a unique GitHub gist that provided all the necessary markdown needed for a comprehensive tutorial! You were able to show your work with descriptive commits. Well done!
+
+            Overall strong effort, keep up the awesome work!
+            </p>
             </Card.Body>
+            <Card.Footer>
+                <Row>
+                <Col>Central Grader, DG</Col>
+                <Col>April 2022</Col>
+                </Row>
+            </Card.Footer>
             </Card>
+                </Col>
+                <Col lg={3} md={4} sm={6}>
+            <Card>
+            <i className=''><BsFillPersonFill size={35} /></i>
+            <Card.Body>
+             <p>
+                Hi Edmer, <br/>
+            Your Regex tutorial on email validation is clean and readable! You were able to explain the purpose and the proper usage of this complex feature! 
+            You were able to provide a clear explanation of the underlying regex topics.  Great job including detailed sections that corresponded to important components in the starter code that make up the regex features. You were able to embed code snippets for each underlying example you used! 
+
+            You created a unique GitHub gist that provided all the necessary markdown needed for a comprehensive tutorial! You were able to show your work with descriptive commits. Well done!
+
+            Overall strong effort, keep up the awesome work!
+            </p>
+            </Card.Body>
+            <Card.Footer>
+                <Row>
+                <Col>Central Grader, DG</Col>
+                <Col>April 2022</Col>
+                </Row>
+            </Card.Footer>
+            </Card>
+                </Col>
+                <Col lg={3} md={4} sm={6}>
+            <Card>
+            <i className=''><BsFillPersonFill size={35} /></i>
+            <Card.Body>
+             <p>
+                Hi Edmer, <br/>
+            Your Regex tutorial on email validation is clean and readable! You were able to explain the purpose and the proper usage of this complex feature! 
+            You were able to provide a clear explanation of the underlying regex topics.  Great job including detailed sections that corresponded to important components in the starter code that make up the regex features. You were able to embed code snippets for each underlying example you used! 
+
+            You created a unique GitHub gist that provided all the necessary markdown needed for a comprehensive tutorial! You were able to show your work with descriptive commits. Well done!
+
+            Overall strong effort, keep up the awesome work!
+            </p>
+            </Card.Body>
+            <Card.Footer>
+                <Row>
+                <Col>Central Grader, DG</Col>
+                <Col>April 2022</Col>
+                </Row>
+            </Card.Footer>
+            </Card>
+                </Col>
+                <Col lg={3} md={4} sm={6}>
+            <Card>
+            <i className=''><BsFillPersonFill size={35} /></i>
+            <Card.Body>
+             <p>
+                Hi Edmer, <br/>
+            Your Regex tutorial on email validation is clean and readable! You were able to explain the purpose and the proper usage of this complex feature! 
+            You were able to provide a clear explanation of the underlying regex topics.  Great job including detailed sections that corresponded to important components in the starter code that make up the regex features. You were able to embed code snippets for each underlying example you used! 
+
+            You created a unique GitHub gist that provided all the necessary markdown needed for a comprehensive tutorial! You were able to show your work with descriptive commits. Well done!
+
+            Overall strong effort, keep up the awesome work!
+            </p>
+            </Card.Body>
+            <Card.Footer>
+                <Row>
+                <Col>Central Grader, DG</Col>
+                <Col>April 2022</Col>
+                </Row>
+            </Card.Footer>
+            </Card>
+                </Col>
+
+            </Row>
         </section>
     )
 }
