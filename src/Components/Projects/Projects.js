28c9e747e537fd7749d2ee7274a4636d956bec8d
edm1001 <edmer_franciz@yahoo.com>
2023-09-10 14:55:08 -0500

    add link to port page

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,42 +1,68 @@
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Wordle from '../../Assets/port/wordle.gif';
 import travelBlog from '../../Assets/port/travel-blog.gif';
 import Chatterize from '../../Assets/port/chatterize.gif';
+import { Container } from 'react-bootstrap';
 
 const Projects = () => {
     return (
-        <section className="portfolio" id='portfolio'>
-            <h2> My Projects</h2>
+        <Container className="project" fluid>
+            <h2 className='text-center'> My Projects</h2>
             <Row className="d-flex justify-content-center project-row">
-                <Col lg={3} md={4} sm={6}>
+
+            <Col lg={3} md={4} sm={6}>
             <Card className="proj-card" to='/portfolio'>
             <Card.Img
             src={Wordle} alt='jo'/>
-            <Card.Body>
-                <button href='/portfolio' to='/portfolio'>Link</button>
-            </Card.Body>
+            <Card.Link href='/portfolio'> link</Card.Link>
             </Card> 
                 </Col>
 
-                <Col lg={3} md={4} sm={6}>
+            <Col lg={3} md={4} sm={6}>
             <Card className="proj-card">
             <Card.Img
             src={Chatterize} alt='jo' />
             </Card> 
-                </Col>
+            </Col>
 
-                <Col lg={3} md={4} sm={6}>
+            <Col lg={3} md={4} sm={6}>
             <Card className="proj-card">
             <Card.Img
             src={travelBlog} alt='jo' />
             </Card> 
+            </Col>
+
+            <Col lg={3} md={4} sm={6}>
+            <Card className="proj-card" to='/portfolio'>
+            <Card.Img
+            src={Wordle} alt='jo'/>
+            <Card.Link href='/portfolio'> link</Card.Link>
+            </Card> 
                 </Col>
+
+            <Col lg={3} md={4} sm={6}>
+            <Card className="proj-card">
+            <Card.Img
+            src={Chatterize} alt='jo' />
+            </Card> 
+            </Col>
+
+            <Col lg={3} md={4} sm={6}>
+            <Card className="proj-card">
+            <Card.Img
+            src={travelBlog} alt='jo' />
+            </Card> 
+            </Col>
+
+            </Row>
+            <Row>
+            <Button  variant="outline-warning" size="lg" className='see-btn' href='/portfolio'> See More! </Button>
             </Row>
 
-
-        </section>
+        </Container>
     )
 
 }
--- src/Pages/Portfolio.js
+++ src/Pages/Portfolio.js
@@ -1,4 +1,8 @@
 import React from "react";
+// import row col and natiner lg=3 md -4 sm =6 =20 featured
+// have a list under the featured proj for "other projects"
+// the projects have a live link, github link, link to the readme
+
 
 const Portfolio = () => {
     return (
