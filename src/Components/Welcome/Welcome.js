3149bdcb9a3da81465eae241d23e412eed09a380
edm1001 <edmer_franciz@yahoo.com>
2023-09-22 19:48:11 -0500

    change welcome comp style

--- src/Components/Welcome/Welcome.js
+++ src/Components/Welcome/Welcome.js
@@ -1,20 +1,23 @@
 import Row from 'react-bootstrap/Row';
 import Col  from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+import Photo from '../../Assets/photos/table.png';
 
 const Welcome =  () => {
     return (
         <section>
             <Row className='hero' id="home">
 
-            <Col className='text-center welcome-pic'>
-            <div>
-                {/* add a intro info box about me, what made me into coding and path i took */}
+            <Col className='text-center bg-info'>
+            <div className='welcome-img'>
+               <Image src={Photo} />
             </div>
             </Col>
 
             <Col className=' text-center ' >
                     <div className='welcome-box'>
-                    <div className='welcome-info text-black py-3 ps-5'>
+                    <div className='welcome-info text-black py-3 ps-5'> 
+                    {/* add a intro info box about me, what made me into coding and path i took */}
                     <h1>color frame</h1>
                     <p>LOREM@KSDNCIAUCKJASNIUANCKJANOI</p>
                     <p>LOREM@KSDNCIAUCKJASNIUANCKJANOI</p>
