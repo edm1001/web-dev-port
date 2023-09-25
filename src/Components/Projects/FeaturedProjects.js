4f93d1c3f11a13ad827a6a391b15287dd569f5d8
edm1001 <edmer_franciz@yahoo.com>
2023-09-25 13:06:40 -0500

    have working modals in FP page

--- src/Components/Projects/FeaturedProjects.js
+++ src/Components/Projects/FeaturedProjects.js
@@ -46,41 +46,40 @@ const FeaturedProjects = () => {
     const handleShow8 = () => setShow8(true);                                                                                                                                                                                                                                                       
     const handleClose9 = () => setShow9(false);
     const handleShow9 = () => setShow9(true);                                                                                                                                                                                                                                                       
+    // const handleClose10 = () => setShow10(false);
+    // const handleShow10 = () => setShow10(true);                                                                                                                                                                                                                                                       
     
     return (
         <Row className=" d-flex justify-content-center py-5">
-        <Col lg={3} md={4} sm={6} >
+        <Col className="py-1" lg={3} md={4} sm={6} >
           <Image  className='w-100 h-100 ftrd-img' src={Wordle} alt="" fluid onClick={handleShow}></Image>
         </Col>
-
-        <Col lg={3} md={4} sm={6} >
+        <Col className="py-1" lg={3} md={4} sm={6} >
           <Image  className='w-100 h-100 ftrd-img' src={Chatterize} alt="" fluid onClick={handleShow2}></Image>
         </Col>
-
-        <Col lg={3} md={4} sm={6} >
+        <Col className="py-1" lg={3} md={4} sm={6} >
           <Image  className='w-100 h-100 ftrd-img' src={TravelDiary} alt="" fluid onClick={handleShow3}></Image>
         </Col>
 
-        <Col lg={3} md={4} sm={6} >
+        <Col className="py-1" lg={3} md={4} sm={6} >
           <Image  className='w-100 h-100 ftrd-img' src={TripleJ} alt="" fluid onClick={handleShow4}></Image>
         </Col>
-
-        <Col lg={3} md={4} sm={6} >
+        <Col className="py-1" lg={3} md={4} sm={6} >
           <Image  className='w-100 h-100 ftrd-img' src={Taskmaster} alt="" fluid onClick={handleShow9}></Image>
         </Col>
-        <Col lg={3} md={4} sm={6} >
+        <Col className="py-1" lg={3} md={4} sm={6} >
           <Image  className='w-100 h-100 ftrd-img' src={IssuesLookup} alt="" fluid onClick={handleShow6}></Image>
         </Col>
-        <Col lg={3} md={4} sm={6} >
+        <Col className="py-1" lg={3} md={4} sm={6} >
           <Image  className='w-100 h-100 ftrd-img' src={PortGenerator} alt="" fluid onClick={handleShow7}></Image>
         </Col>
-        <Col lg={3} md={4} sm={6} >
+        <Col className="py-1" lg={3} md={4} sm={6} >
           <Image  className='w-100 h-100 ftrd-img' src={SalonSite} alt="" fluid onClick={handleShow8}></Image>
         </Col>
-        <Col lg={3} md={4} sm={6} >
+        <Col className="py-1" lg={3} md={4} sm={6} >
           <Image  className='w-100 h-100 ftrd-img' src={DailyScheduler} alt="" fluid onClick={handleShow5}></Image>
         </Col>
-        <Col lg={3} md={4} sm={6} >
+        <Col className="py-1" lg={3} md={4} sm={6} >
           <Image  className='w-100 h-100 ftrd-img' src={Zookeepers} alt="" fluid onClick={handleShow9}></Image>
         </Col>
 
@@ -133,18 +132,80 @@ const FeaturedProjects = () => {
         </Modal.Footer>
         </Modal>
 
-        <Modal show={show5} onHide={handleClose4}>
+        <Modal show={show5} onHide={handleClose5}>
         <Modal.Header closeButton>
         <Modal.Title>Modal heading</Modal.Title>
         </Modal.Header>
         <Modal.Body>You are reading this text in a modal5!</Modal.Body>
         <Modal.Footer>
-        <Button variant="outline-success" onClick={handleClose4}>
+        <Button variant="outline-success" onClick={handleClose5}>
             Close
         </Button>
         </Modal.Footer>
         </Modal>
- 
+      
+        <Modal show={show6} onHide={handleClose6}>
+        <Modal.Header closeButton>
+        <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>You are reading this text in a modal6!</Modal.Body>
+        <Modal.Footer>
+        <Button variant="outline-success" onClick={handleClose6}>
+            Close
+        </Button>
+        </Modal.Footer>
+        </Modal>
+
+        <Modal show={show7} onHide={handleClose7}>
+        <Modal.Header closeButton>
+        <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>You are reading this text in a modal7!</Modal.Body>
+        <Modal.Footer>
+        <Button variant="outline-success" onClick={handleClose7}>
+            Close
+        </Button>
+        </Modal.Footer>
+        </Modal>
+
+        <Modal show={show8} onHide={handleClose8}>
+        <Modal.Header closeButton>
+        <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>You are reading this text in a modal8!</Modal.Body>
+        <Modal.Footer>
+        <Button variant="outline-success" onClick={handleClose8}>
+            Close
+        </Button>
+        </Modal.Footer>
+        </Modal>
+
+        <Modal show={show9} onHide={handleClose9}>
+        <Modal.Header closeButton>
+        <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>You are reading this text in a modal9!</Modal.Body>
+        <Modal.Footer>
+        <Button variant="outline-success" onClick={handleClose9}>
+            Close
+        </Button>
+        </Modal.Footer>
+        </Modal>
+
+        {/* <Modal show={show10} onHide={handleClose10}>
+        <Modal.Header closeButton>
+        <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>You are reading this text in a modal6!</Modal.Body>
+        <Modal.Footer>
+        <Button variant="outline-success" onClick={handleClose10}>
+            Close
+        </Button>
+        </Modal.Footer>
+        </Modal> */}
+
+
+
     </Row>
     )
 };
