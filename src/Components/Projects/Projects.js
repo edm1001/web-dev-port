3c1600ed60fb6917208ad9c1b830fdd961ae8147
edm1001 <edmer_franciz@yahoo.com>
2023-10-03 17:53:53 -0500

    make mobile styles for proj section

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,7 +1,7 @@
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import Image from 'react-bootstrap/Image';
 import Wordle from '../../Assets/port/wordle.gif';
 import PortGenerator from '../../Assets/port/portfolio=generator.gif';
 import Chatterize from '../../Assets/port/chatterize.gif';
@@ -13,26 +13,21 @@ const Projects = () => {
             <h2 className='text-center pt-5'> My Projects</h2>
             <Row className="d-flex justify-content-center p-5 project-row">
 
-            <Col className='' lg={4} md={4} sm={6}>
-            <Card className="proj-card h-90 w-85" to='/portfolio'>
-            <Card.Img
-            src={Wordle} alt='jo'/>
-            </Card> 
+            <Col className='p-2' lg={4} md={4} sm={4}>
+            <Image className="proj-img h-90 w-85" as='a'  src={Wordle} to='/portfolio'>
+            </Image> 
                 </Col>
 
-            <Col lg={4} md={4} sm={6}>
-            <Card className="proj-card h-90 w-85">
-            <Card.Img
-            src={Chatterize} alt='jo' />
-            </Card> 
-            </Col>
+            <Col className='p-2' lg={4} md={4} sm={4}>
+            <Image className="proj-img h-90 w-85" as='a'  src={Chatterize} to='/portfolio'>
+            </Image> 
+                </Col>
 
-            <Col lg={4} md={4} sm={6}>
-            <Card className="proj-card h-90 w-85">
-            <Card.Img
-            src={PortGenerator} alt='jo' />
-            </Card> 
-            </Col>
+            <Col className='p-2' lg={4} md={4} sm={4}>
+            <Image className="proj-img h-90 w-85" as='a'  src={PortGenerator} to='/portfolio'>
+            </Image> 
+                </Col>
+
             </Row >
 
             <div className='pb-5 text-center'>
