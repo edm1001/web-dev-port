12bc5addf040b5c4ae4b38697cd6c96224f05a85
edm1001 <edmer_franciz@yahoo.com>
2023-10-17 19:50:04 -0500

    initial work for contact form

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,10 +1,56 @@
 import {Row, Col, Button, Form } from 'react-bootstrap';
+import {useRef, useState} from 'react';
+
 import {FcGoogle} from 'react-icons/fc';
 import {FaGithubAlt} from 'react-icons/fa';
+import { validateEmail } from '../../utils/helpers';
 
+// import {emailjs} from '@emailjs/browser';
 
 
 const Contact = () => {
+
+  // const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+  // const [errorMessage, setErrorMessage] = useState('');
+  //   const { name, email, message, subject } = formState;
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   if (!errorMessage) {
+  //     setFormState({ [e.target.name]: e.target.value });
+  //     console.log('Form', formState);
+  //   }
+  // };
+
+  //   const handleChange = (e) => {
+  //       if (e.target.name === 'email') {
+  //         const isValid = validateEmail(e.target.value);
+  //         if (!isValid) {
+  //           setErrorMessage('Your email is invalid.');
+  //         } else {
+  //           setErrorMessage('');
+  //         }
+  //       } else {
+  //         if (!e.target.value.length) {
+  //           setErrorMessage(`${e.target.name} is required.`);
+  //         } else {
+  //           setErrorMessage('');
+  //         }
+  //       }
+  //     };
+  //     const form = useRef();
+
+  //     const sendEmail = (e) => {
+  //       e.preventDefault();
+    
+  //       emailjs.sendForm('personal_contact','contact_service', form.current, 'wLYbtp8s7HHiOK8mX')
+  //         .then((result) => {
+  //             console.log(result.text);
+  //         }, (error) => {
+  //             console.log(error.text);
+  //         });
+
+
     return (
         <section className='contact' id='contact'>
 
@@ -34,18 +80,18 @@ const Contact = () => {
                     <Row >
                         <Col>
                         <Form.Label>Name</Form.Label>
-                        <Form.Control required type='name' placeholder='Enter Name'></Form.Control></Col>
+                        <Form.Control required  name='suser_name' type='name' defaultValue={name} onBlur={handleChange} placeholder='Enter Name'></Form.Control></Col>
                         <Col>
                         <Form.Label>Subject</Form.Label>
-                        <Form.Control type='subject' placeholder='Subject here'></Form.Control></Col>
+                        <Form.Control type='subject' name='subject' defaultValue={subject} onBlur={handleChange} placeholder='Subject here'></Form.Control></Col>
                     </Row>
                     <Form.Label htmlFor=''>Email</Form.Label>
-                    <Form.Control type='email' placeholder='Enter your email'></Form.Control>
+                    <Form.Control type='email' name='user_email' defaultValue={email} onBlur={handleChange} placeholder='Enter your email'></Form.Control>
                     <Form.Label>Message</Form.Label>
-                    <Form.Control as='textarea' rows={4} placeholder='Write your message here'>
+                    <Form.Control name='message' defaultValue={message} onBlur={handleChange}  as='textarea' rows={4} placeholder='Write your message here'>
                     </Form.Control>
                 </Form.Group>
-                <Button type='submit' id='contact-btn'> Submit</Button>
+                <Button type='submit' value='submit' id='contact-btn'> Submit</Button>
             </Form>
             </Col>
             
@@ -54,5 +100,6 @@ const Contact = () => {
         </section>
     )
 }
+// }
 
 export default Contact;
