70c9cea3848efdd0e477608ddb31a843052ed67f
edm1001 <edmer_franciz@yahoo.com>
2023-10-18 21:59:21 -0500

    fix navbar spacing

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -2,13 +2,14 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
-import Logo from '../../Assets/videos/port-logo.gif'
+import Logo from '../../Assets/videos/port-logo.gif';
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
 
 const Header = () => {
     return (
       <Navbar className="" sticky="top" bg="dark" data-bs-theme="dark" expand="lg" >
-        <Container>
-        <div className='ps-3'>
+        <div className='ps-5'>
         <Navbar.Brand  href='/' classsName='nav-logo'>
           <img 
           src={Logo}
@@ -20,7 +21,8 @@ const Header = () => {
         </div>
           <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
           <Navbar.Collapse id="" >
-          <Nav className="nav-links pt-3 text-center">
+        <Container className='bg-dark'>
+          <Nav className="nav-links py-2">
             <Nav.Link href="/" activeStyle>Home</Nav.Link>
             <Nav.Link href="/#about">About me</Nav.Link>
             <Nav.Link href="/portfolio">Portfolio</Nav.Link>            
@@ -28,11 +30,11 @@ const Header = () => {
             <Nav.Link href="/#contact">Contact</Nav.Link>
             <Nav.Link href="/#reviews">Reviews</Nav.Link>
           </Nav>
-        <div className='text-center py-2'>
-            <Button href='/ResumePdf' variant='outline-danger' className=''>Resume</Button>
+      </Container>
+        <div className='text-center px-3'>
+            <Button href='/ResumePdf' variant='outline-secondary' className=''>Resume</Button>
         </div>
           </Navbar.Collapse>       
-      </Container>
         </Navbar>
     )
 }
