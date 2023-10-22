ba67abd4340ab7f9dce6726f628930d5c9902825
edm1001 <edmer_franciz@yahoo.com>
2023-10-22 17:25:43 -0500

    enter info for abt comp

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -11,18 +11,18 @@ const About = () => {
       <Container>
         <Row className="abt-info">
 
-            <Col lg={6} md={4} sm={12}>
+            <Col lg={6} md={6} sm={12}>
             <div className='about-img h-100 w-100 my-4 p-4'>
             <img  alt='about-pic' src={AboutPic} className='h-100 w-100'/>
             </div>
             </Col>
 
 
-            <Col className="my-5 py-2 " lg={6} md={8} sm={12}>
+            <Col className="my-5 py-2 " lg={6} md={6} sm={12}>
             <div className=" p-2 about-msg">
             <h4 className="fw-bold"> Why Hire Me?</h4>
         <p className="fs-6">
-            My name is Edmer Valencia, I am a Full Stack Developer that graduated from the coding bootcamp program offered in University of Texas at Austin. I am eager to learn and create various technologies and projects.
+            I am passionate for Web Development, I love it because its a career that feeds my creativity and allows me to bring fresh ideas into projects and learn ideas from amazing people. Hire me because I am a tech geek with a proven ability to work with people because of my past work experience in retail, freelance and in the coding bootcamp. I am confident that I will contribute positively to the company as I am committed to pursue learning the continuing evolution of Web Development. 
         </p>  
              <Row className="about-badge">
                 <Col className="text-start ps-3 fs-small">
