d72587dcb24c239111f7d23029a319dd96eccd27
Edmer Valencia <edmerfranciz@gmail.com>
2024-10-02 17:17:35 -0500

    edit about page

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,70 +1,77 @@
-import Container from "react-bootstrap/Container"
+import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Badge from "react-bootstrap/Badge";
-import AboutPic from '../../Assets/photos/personcoding.png'
+import AboutPic from "../../Assets/photos/personcoding.png";
 import { motion } from "framer-motion";
 
 const About = () => {
-    return (
-    
-    <section className="about" id="about">
-      <Container>
-        <Row className="abt-info">
-            <Col lg={6} md={5} sm={12} xs={12} >
-            <motion.div
-            initial={{x:-200 ,opacity:0}}
-            whileInView={{x:0, opacity:1, animationDelay:2}}
-            >
-            <div className='about-img px-4'>
-            <img  alt='about-pic' src={AboutPic} className='w-80'/>
-            </div>
-            </motion.div>
-            </Col>
+  return (
+<section className="about" id="about">
+  <Container>
+    <Row className="abt-info">
+      <Col lg={6} md={5} sm={12} xs={12}>
+        <motion.div
+          initial={{ x: -200, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1, animationDelay: 2 }}
+        >
+          <div className="about-img px-4 text-center text-md-start">
+            <img
+              alt="about-pic"
+              src={AboutPic}
+              className="img-fluid"
+              style={{ maxWidth: '100%', height: 'auto' }}
+            />
+          </div>
+        </motion.div>
+      </Col>
 
-
-        <Col className="pb-5 " lg={6} md={7} sm={12} xs={12}>
-            <motion.div
-                initial={{x:200 ,opacity:0}}
-                whileInView={{x:0, opacity:1, }}
-                transition={{
-                    delay:0.5,
-                }}>
-            <h4 
-             className="fw-semibold text-dark fs-3 text-center"
-            >
+      <Col className="pb-5" lg={6} md={7} sm={12} xs={12}>
+        <motion.div
+          initial={{ x: 200, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{
+            delay: 0.5,
+          }}
+        >
+          <h4 className="fw-semibold text-dark fs-3 text-center text-md-start">
             About Me
-            </h4>
-            <div className=" p-2 about-msg">
-            <p className="p-1" id='abt-msg' >
-            I am enthusiastic about Web Development, this field combines creativity with service that I am passionate towards. With my background in retail, freelance work, and coding bootcamp, I've honed my ability to collaborate effectively and commited to ongoing learning. I will ensure to positively contribute to your company's success.
-            </p>  
-        </div>
-        <Row className="about-badge my-3 text-black">
-                <Col className="text-start ps-4">
-                <p id='abt-msg'>
-                <Badge bg="success" >Ethnicity :</Badge> Filipino <br/>
-                <Badge bg="success">Birthday :</Badge> October 1, 1998 <br/>
-                <Badge bg='success'>Age :</Badge> 25 <br/>
-                </p>
-                </Col>
-                <Col className="text-start pe-5" id='abt-msg'>
-                <p className="text-start" id='abt-msg'>
-                <Badge bg="success" >Birthplace : </Badge> Iloilo City, Philippines <br/>
-                <Badge bg="success">School :</Badge> Coding bootcamp at UT Austin<br/>
-                <Badge bg='success'>Loves :</Badge> Tech, Nature, & Music<br/>
-                </p> 
-                </Col>
-            </Row>
-            </motion.div>
+          </h4>
+          <div className="about-msg">
+            <p className="p-2" id="abt-msg">
+              I am enthusiastic about Web Development, this field combines
+              creativity with service that I am passionate towards. With my
+              background in retail, freelance work, and coding bootcamp, I've
+              honed my ability to collaborate effectively and am committed to
+              ongoing learning. I will ensure to positively contribute to your
+              company's success.
+            </p>
+          </div>
+
+          <Row className="about-badge my-3 text-black">
+            <Col className="text-start ps-4">
+              <p id="abt-msg">
+                <Badge bg="success">Ethnicity :</Badge> Filipino <br />
+                <Badge bg="success">Birthday :</Badge> October 1, 1998 <br />
+                <Badge bg="success">Age :</Badge> 25 <br />
+              </p>
             </Col>
+            <Col className="text-start pe-5" id="abt-msg">
+              <p id="abt-msg">
+                <Badge bg="success">Birthplace :</Badge> Iloilo City,
+                Philippines <br />
+                <Badge bg="success">School :</Badge> Coding bootcamp at UT
+                Austin <br />
+                <Badge bg="success">Loves :</Badge> Tech, Nature, & Music <br />
+              </p>
+            </Col>
+          </Row>
+        </motion.div>
+      </Col>
+    </Row>
+  </Container>
+</section>
 
-            
-        </Row>
-    
-            </Container>            
-        </section>
-
-    )
-}
+  );
+};
 export default About;
--- src/Components/Welcome/Welcome.js
+++ src/Components/Welcome/Welcome.js
@@ -1,51 +1,67 @@
-import {motion} from 'framer-motion';
-import Row from 'react-bootstrap/Row';
-import Col  from 'react-bootstrap/Col';
-import Image from 'react-bootstrap/Image';
-import Photo from '../../Assets/photos/personal/clr-self.png';
+import { motion } from "framer-motion";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
+import Photo from "../../Assets/photos/personal/clr-self.png";
 
-const Welcome =  () => {
-    return (
-<section>
-    <Row className='hero' id="home">
+const Welcome = () => {
+  return (
+    <section>
+      <Row className="w-100" id="home">
         {/* Text Column */}
-        <Col lg={6} md={6} sm={12} xs={12} className='text-center bg-dark'>
-            <motion.div
-                initial={{ x: -200 }}
-                whileInView={{ x: -25 }}              
-                className='welcome-img px-4 py-5' // Removed extra margin
-            >
-                <h1 className='fw-bold text-warning text-start ps-lg-5 ps-2'>Hello!</h1> {/* Adjust padding for mobile */}
-                <p className='text-white text-start ps-lg-5 ps-2'> {/* Same padding adjustment */}
-                    My name is Edmer Valencia, and I am a Full Stack Developer with a background in web development, having completed the coding bootcamp at the University of Texas at Austin.
-                </p>
-                <p className='text-white text-start ps-lg-5 ps-2'>
-                    I am passionate about learning and applying my skills to develop innovative technologies and dynamic projects.
-                </p>
-            </motion.div>
+        <Col lg={6} md={6} sm={12} xs={12} className="text-center bg-dark">
+          <motion.div
+            initial={{ x: -200 }}
+            whileInView={{ x: -25 }}
+            className="welcome-img px-4 py-5"
+          >
+            <h1 className="fw-bold text-warning text-start ps-lg-5 ps-2">
+              Hello!
+            </h1>{" "}
+            <p className="text-white text-start ps-md-1 pt-1">
+              {" "}
+              My name is Edmer Valencia, and I am a Full Stack Developer with a
+              background in web development, having completed the coding
+              bootcamp at the University of Texas at Austin.
+            </p>
+            <p className="text-white text-start ps-md-1 ">
+              I am passionate about learning and applying my skills to develop
+              innovative technologies and dynamic projects.
+            </p>
+          </motion.div>
         </Col>
 
         {/* Image Column */}
-        <Col lg={6} md={6} sm={12} xs={12} className='bg-dark d-flex justify-content-center align-items-center pb-4 overflow-hidden'> {/* Flexbox for centering */}
+        <Col
+          lg={6}
+          md={6}
+          sm={12}
+          xs={12}
+          className="bg-dark d-flex justify-content-center align-items-center pb-4 overflow-hidden"
+        >
+          {" "}
+          <motion.div
+            initial={{ x: 200 }}
+            whileInView={{ x: 40 }}
+            transition={{ delay: 1 }}
+            className="welcome-box w-100"
+            style={{ maxWidth: "100%", width: "100%" }}
+          >
             <motion.div
-                initial={{ x: 200 }}
-                whileInView={{ x: 40 }}
-                transition={{ delay: 1 }} 
-                className='welcome-box'
-                style={{ maxWidth: '100%', width: '100%' }}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 2 }}
             >
-                <motion.div
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    transition={{ delay: 2 }}
-                >
-                    <Image src={Photo} id='personal-photo' className='img-fluid rounded-circle' /> {/* Added img-fluid for responsive image */}
-                </motion.div>
+              <Image
+                src={Photo}
+                id="personal-photo"
+                className="img-fluid rounded-circle"
+              />{" "}
             </motion.div>
+          </motion.div>
         </Col>
-    </Row>
-</section>
-
-    )
-}
+      </Row>
+    </section>
+  );
+};
 export default Welcome;
