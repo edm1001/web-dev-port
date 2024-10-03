ee50c3ab42dca74ba4c98294c34fe4047d378586
Edmer Valencia <edmerfranciz@gmail.com>
2024-10-02 23:58:25 -0500

    fix logo animation

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -1,190 +1,226 @@
-import React, { useState, useEffect } from 'react';
-import {motion} from 'framer-motion';
-import Container from 'react-bootstrap/Container';
-import Card from 'react-bootstrap/Card';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import ProgressBar from 'react-bootstrap/ProgressBar';
-import {FaBootstrap, FaReact, FaWordpress} from 'react-icons/fa';
-import {IoLogoCss3, IoLogoJavascript} from 'react-icons/io';
-import {VscJson} from 'react-icons/vsc';
-import {DiNpm, DiNodejs,  DiResponsive} from 'react-icons/di';
-import {AiOutlineHtml5, AiFillGithub, AiFillApi} from 'react-icons/ai';
-import {SiMongodb, SiExpress, SiHeroku, SiFramer, SiJquery, SiSequelize} from 'react-icons/si';
-import {BsFiletypeSql} from 'react-icons/bs';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import ProgressBar from "react-bootstrap/ProgressBar";
+import { FaBootstrap, FaReact, FaWordpress } from "react-icons/fa";
+import { IoLogoCss3, IoLogoJavascript } from "react-icons/io";
+import { VscJson } from "react-icons/vsc";
+import { DiNpm, DiNodejs, DiResponsive } from "react-icons/di";
+import { AiOutlineHtml5, AiFillGithub, AiFillApi } from "react-icons/ai";
+import {
+  SiMongodb,
+  SiExpress,
+  SiHeroku,
+  SiFramer,
+  SiJquery,
+  SiSequelize,
+} from "react-icons/si";
+import { BsFiletypeSql } from "react-icons/bs";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-  const serviceCards = [
-    {
-        header: 'DSA',
-        title: 'Data Structure & Algorithms',
-        description: 'My knowledge of DSA enables me to efficiently manage and analyze data through structured scripting and algorithmic processes.'
-    },
-    {
-        header: 'OOP',
-        title: 'Object Oriented Programming',
-        description: 'A popular paradigm to create easy to refractor sites using reusable code blocks and object-relational mapping.'
-    },
-    {
-        header: 'MVC',
-        title: 'Model View Controller',
-        description: 'A framework pattern for computer softwares that easilhy distinguish related program logic into model, view, and controller elements.'
-    },
-    {
-        header: 'PWA',
-        title: 'Progressive Web Application',
-        description: 'PWAs work on any platform with a compliant browser, offering a seamless user experience like native apps. They use service workers for offline access, push notifications, and enhanced performance.'
-    },
-    {
-        header: 'CSM',
-        title: 'Client-Server Model',
-        description: 'Experienced in the client-server model, I specialize in architecting networked applications for seamless communication. Whether optimizing web interactions or designing efficient distributed systems, my proficiency is crucial for crafting effective and responsive software solutions.'
-    }
-  ]
+const serviceCards = [
+  {
+    header: "DSA",
+    title: "Data Structure & Algorithms",
+    description:
+      "My knowledge of DSA enables me to efficiently manage and analyze data through structured scripting and algorithmic processes.",
+  },
+  {
+    header: "OOP",
+    title: "Object Oriented Programming",
+    description:
+      "A popular paradigm to create easy to refractor sites using reusable code blocks and object-relational mapping.",
+  },
+  {
+    header: "MVC",
+    title: "Model View Controller",
+    description:
+      "A framework pattern for computer softwares that easilhy distinguish related program logic into model, view, and controller elements.",
+  },
+  {
+    header: "PWA",
+    title: "Progressive Web Application",
+    description:
+      "PWAs work on any platform with a compliant browser, offering a seamless user experience like native apps. They use service workers for offline access, push notifications, and enhanced performance.",
+  },
+  {
+    header: "CSM",
+    title: "Client-Server Model",
+    description:
+      "Experienced in the client-server model, I specialize in architecting networked applications for seamless communication. Whether optimizing web interactions or designing efficient distributed systems, my proficiency is crucial for crafting effective and responsive software solutions.",
+  },
+];
 
-  const techIcons = [
-    { Component: AiOutlineHtml5, title: 'HTML5' },
-    { Component: IoLogoCss3, title: 'CSS3' },
-    { Component: IoLogoJavascript, title: 'JavaScript' },
-    { Component: FaBootstrap, title: 'Bootstrap' },
-    { Component: SiJquery, title: 'JQuery' },
-    { Component: FaReact, title: 'React' },
-    { Component: VscJson, title: 'JSON' },
-    { Component: DiNodejs, title: 'NodeJS' },
-    { Component: DiNpm, title: 'NPM' },
-    { Component: SiMongodb, title: 'MongoDB' },
-    { Component: SiExpress, title: 'Express Router' },
-    { Component: DiResponsive, title: 'Responsive Media' },
-    { Component: AiFillGithub, title: 'GitHub' },
-    { Component: BsFiletypeSql, title: 'SQL' },
-    { Component: AiFillApi, title: 'APIs' },
-    { Component: SiHeroku, title: 'Heroku Cloud' },
-    { Component: SiFramer, title: 'Framer Motion' },
-    { Component: FaWordpress, title: 'WordPress' },
-  ];
+const techIcons = [
+  { Component: AiOutlineHtml5, title: "HTML5" },
+  { Component: IoLogoCss3, title: "CSS3" },
+  { Component: IoLogoJavascript, title: "JavaScript" },
+  { Component: FaBootstrap, title: "Bootstrap" },
+  { Component: SiJquery, title: "JQuery" },
+  { Component: FaReact, title: "React" },
+  { Component: VscJson, title: "JSON" },
+  { Component: DiNodejs, title: "NodeJS" },
+  { Component: DiNpm, title: "NPM" },
+  { Component: SiMongodb, title: "MongoDB" },
+  { Component: SiExpress, title: "Express Router" },
+  { Component: DiResponsive, title: "Responsive Media" },
+  { Component: AiFillGithub, title: "GitHub" },
+  { Component: BsFiletypeSql, title: "SQL" },
+  { Component: AiFillApi, title: "APIs" },
+  { Component: SiHeroku, title: "Heroku Cloud" },
+  { Component: SiFramer, title: "Framer Motion" },
+  { Component: FaWordpress, title: "WordPress" },
+];
 
 const ExpandableText = ({ children, descriptionLength }) => {
-    const fullText = children;
-    const [isExpanded, setIsExpanded] = useState(false);
+  const fullText = children;
+  const [isExpanded, setIsExpanded] = useState(false);
 
-    const toggleText = () => {
-      setIsExpanded(!isExpanded);
-    };
-  
-    return (
-      <p className='text'>
-        {isExpanded ? fullText : `${fullText.slice(0, descriptionLength)}...`}
-        <span onClick={toggleText} className='toggle-button'>
-          {isExpanded ? 'Show less' : 'Show more'}
-        </span>
-      </p>
-    );
+  const toggleText = () => {
+    setIsExpanded(!isExpanded);
   };
-  const renderIcons = () => {
-    return techIcons.map(({ Component, title }, index) => (
-      <motion.div
-        key={index}
-        initial={{ opacity: 0, scale: 0.8 }}
-        whileInView={{ opacity: 1, scale: 1 }}
-        whileHover={{ scale: 1.2 }}
-        transition={{ duration: 0.5 }}
-        className="cursor-pointer mx-3 my-2"
-      >
-        <Component title={title} size={50} color="lightgrey" className="skl-logo" />
-      </motion.div>
-    ));
-  };
+
+  return (
+    <p className="text">
+      {isExpanded ? fullText : `${fullText.slice(0, descriptionLength)}...`}
+      <span onClick={toggleText} className="toggle-button">
+        {isExpanded ? "Show less" : "Show more"}
+      </span>
+    </p>
+  );
+};
+const renderIcons = () => {
+  return techIcons.map(({ Component, title }, index) => (
+    <motion.div
+      key={index}
+      initial={{ opacity: 0, scale: 0.8 }}
+      whileInView={{ opacity: 1, scale: 1 }}
+      whileHover={{ scale: 1.2 }}
+      transition={{ duration: 0.5 }}
+      className="cursor-pointer mx-3 my-2"
+    >
+      <Component
+        title={title}
+        size={50}
+        color="lightgrey"
+        className="skl-logo"
+      />
+    </motion.div>
+  ));
+};
 
 const Services = () => {
-    const [currentSlide, setCurrentSlide] = useState(0)
-    
+  const [currentSlide, setCurrentSlide] = useState(0);
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setCurrentSlide((prevSlide) => (prevSlide + 1) % serviceCards.length)
-        }, 2500);
-        return () => clearInterval(interval);
-    }, []);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentSlide((prevSlide) => (prevSlide + 1) % serviceCards.length);
+    }, 2500);
+    return () => clearInterval(interval);
+  }, []);
 
-    const settings = {
-        infinite: true,
-        speed: 8000,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        initialSlide: 0,
-        beforeChange: (current, next) => setCurrentSlide(next),
-        autoplay: true,
-        arrows: false,
-    }
+  const settings = {
+    infinite: true,
+    speed: 8000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    beforeChange: (current, next) => setCurrentSlide(next),
+    autoplay: true,
+    arrows: false,
+  };
 
-    return (
-        <section className='services' id='services'>
-            <Container className='h-100 w-100'>
-
-
-            <Row className='srvc-row' bg='dark' data-bs-theme='dark'>
-
-            <Col lg={6} md={12} sm={12} >
-                <div className=''>
-                    <h2 className='text-warning fw-bold'> Services</h2>
-                <Row className='text-start fw-bold py-3 my-5 progress-row'>
-                    <Col className='col-12 py-1 text-start'>
-                    <h6>   Full Stack Development:</h6>
-                    <ProgressBar animated variant='warning' now={100} className='' />
-                    <small className='text-muted'>React, Node.js, Express, MongoDB</small>
-                    </Col>
-                    <Col className='col-12 py-1 text-start'>
-                    <h6>MERN Stack:</h6>
-                    <ProgressBar animated variant='warning' now={100} className='' />
-                    <small className='text-muted'>MongoDB, Express, React, Node.js</small>
-                    </Col>
-                    <Col className='col-12 py-1'>
-                    <h6>CRUD Operations:</h6>
-                    <ProgressBar animated variant='warning' now={100} className='' />
-                    <small className='text-muted'>Database interactions, RESTful APIs</small>
-                    </Col>
-                </Row>
-                </div>
+  return (
+    <section className="services" id="services">
+      <Container className="h-100 w-100">
+        <Row className="srvc-row" bg="dark" data-bs-theme="dark">
+          <Col lg={6} md={12} sm={12}>
+            <div className="">
+              <h2 className="text-warning fw-bold"> Services</h2>
+              <Row className="text-start fw-bold py-3 my-5 progress-row">
+                <Col className="col-12 py-1 text-start">
+                  <h6> Full Stack Development:</h6>
+                  <ProgressBar
+                    animated
+                    variant="warning"
+                    now={100}
+                    className=""
+                  />
+                  <small className="text-muted">
+                    React, Node.js, Express, MongoDB
+                  </small>
                 </Col>
+                <Col className="col-12 py-1 text-start">
+                  <h6>MERN Stack:</h6>
+                  <ProgressBar
+                    animated
+                    variant="warning"
+                    now={100}
+                    className=""
+                  />
+                  <small className="text-muted">
+                    MongoDB, Express, React, Node.js
+                  </small>
+                </Col>
+                <Col className="col-12 py-1">
+                  <h6>CRUD Operations:</h6>
+                  <ProgressBar
+                    animated
+                    variant="warning"
+                    now={100}
+                    className=""
+                  />
+                  <small className="text-muted">
+                    Database interactions, RESTful APIs
+                  </small>
+                </Col>
+              </Row>
+            </div>
+          </Col>
 
-            <Col lg={6} md={12} sm={12} >
-            <Row className='my-3 mx-2'>
-                <Col lg={12} md={12} sm={12} xs={12} className='srvc-col'>
-                    <Slider {...settings}>
-                    {serviceCards.map((serviceCard, index) => (                      
+          <Col lg={6} md={12} sm={12}>
+            <Row className="my-3 mx-2">
+              <Col lg={12} md={12} sm={12} xs={12} className="srvc-col">
+                <Slider {...settings}>
+                  {serviceCards.map((serviceCard, index) => (
                     <div key={index}>
-                    <Card className='w-100 h-100 mt-3' border='secondary' >
-                    <Card.Header className='srvc-header text-center align-center'> <h5>{serviceCard.header}</h5></Card.Header>
-                    <Card.Body>
-                    <Card.Title className='fw-small fs-6 text-warning'>{serviceCard.title}</Card.Title>
-                    <p>
-                    <ExpandableText descriptionLength={150}>
-                    {serviceCard.description}
-                    </ExpandableText>
-                    </p>
-                    </Card.Body>
-                    </Card>                     
+                      <Card className="w-100 h-100 mt-3" border="secondary">
+                        <Card.Header className="srvc-header text-center align-center">
+                          {" "}
+                          <h5>{serviceCard.header}</h5>
+                        </Card.Header>
+                        <Card.Body>
+                          <Card.Title className="fw-small fs-6 text-warning">
+                            {serviceCard.title}
+                          </Card.Title>
+                          <p>
+                            <ExpandableText descriptionLength={150}>
+                              {serviceCard.description}
+                            </ExpandableText>
+                          </p>
+                        </Card.Body>
+                      </Card>
                     </div>
-                ))}
+                  ))}
                 </Slider>
-                </Col>
+              </Col>
             </Row>
-            </Col>
-        
-            </Row>
+          </Col>
+        </Row>
+      </Container>
 
-            </Container>
-
-
-     <div className="tech-learned py-2">
+      <div className="tech-learned py-2">
         <h4 className="fs-2 text-warning fw-bold">My Stack</h4>
         <div className="d-flex flex-wrap justify-content-center">
           {renderIcons()}
         </div>
       </div>
-        </section>
-    )
-}
+    </section>
+  );
+};
 export default Services;
--- src/Pages/Portfolio.js
+++ src/Pages/Portfolio.js
@@ -1,53 +1,72 @@
 import Container from "react-bootstrap/Container";
-import Row from 'react-bootstrap/Row'
+import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import FeaturedProjects from "../Components/Projects/FeaturedProjects";
-import ListProjects from '../Components/Projects/ListProjects';
-import Contact from '../Components/Contact/Contact'
+import ListProjects from "../Components/Projects/ListProjects";
+import Contact from "../Components/Contact/Contact";
 import { AiFillGithub } from "react-icons/ai";
 import { motion } from "framer-motion";
 
 const Portfolio = () => {
- 
-    return (
-        <section className="portfolio">
-        <Container className="portfolio mb-5">
-        <h1 className='mt-3 text-warning align-center text-center fw-bold'>Projects Page</h1>
-        <Row as='div'className="mt-5 "> 
-            <Col lg={6} md={6} sm={12} xs={12}>
-                <motion.h4 
-                  initial={{opacity:0.2}}
-                  whileInView={{opacity:1}}
-                  className='text-secondary fs-3 text-center pt-3'
-                  >Link to my Github:</motion.h4>
-            </Col>
-            <Col  className='text-center' lg={6} md={6} sm={12} xs={12}>
-             <motion.div
-                whileHover={{
-                    scale:1.3, 
-                    x:75
-                }}
-                >
-                <a
-                className='text-success cursor-pointer' 
-                href='https://github.com/edm1001?tab=repositories'
-                target="_blank" 
-                rel="noopener noreferrer"                
-                >
-                <AiFillGithub 
-                size={70}/>
-                </a>
+  return (
+    <section className="portfolio">
+      <Container className="portfolio mb-5">
+        <h1 className="mt-3 text-warning align-center text-center fw-bold">
+          Projects Page
+        </h1>
+        <Row
+          as="div"
+          className="mt-5 d-flex align-items-center justify-content-center"
+        >
+          <Col
+            lg={6}
+            md={6}
+            sm={12}
+            xs={12}
+            className="d-flex justify-content-center"
+          >
+            <motion.h4
+              initial={{ opacity: 0.2 }}
+              whileInView={{ opacity: 1 }}
+              className="text-secondary fs-3 text-center pt-3"
+            >
+              Link to my Github:
+            </motion.h4>
+          </Col>
+          <Col
+            lg={6}
+            md={6}
+            sm={12}
+            xs={12}
+            className="text-center d-flex justify-content-center"
+          >
+            <motion.div
+              whileHover={{
+                scale: 1.3,
+                x: 0, // Keep horizontal movement zero for better centering on hover
+              }}
+            >
+              <a
+                className="text-success cursor-pointer"
+                href="https://github.com/edm1001?tab=repositories"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <AiFillGithub size={70} />
+              </a>
             </motion.div>
-            </Col>
-        </Row >
-        <Row><FeaturedProjects/></Row>
-        <Row><ListProjects/></Row>
-        </Container>
-        <Contact/>
-
-        
-        </section>
-    )
-}
+          </Col>
+        </Row>
+        <Row>
+          <FeaturedProjects />
+        </Row>
+        <Row>
+          <ListProjects />
+        </Row>
+      </Container>
+      <Contact />
+    </section>
+  );
+};
 
 export default Portfolio;
