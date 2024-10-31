9f472f1807a4d6378ad8674954659c3a265406a9
Edmer Valencia <edmerfranciz@gmail.com>
2024-10-31 00:19:08 -0500

    update nav

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,41 +1,48 @@
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
-import Logo from '../../Assets/videos/port-logo.gif';
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import Logo from "../../Assets/videos/port-logo.gif";
 
 const Header = () => {
-    return (
-      <Navbar className="" sticky="top" bg="dark" data-bs-theme="dark" expand="lg" >
-        <div className='ps-5'>
-        <Navbar.Brand  href='/' classsName='nav-logo'>
-          <img 
-          src={Logo}
-          width="80"
-          height="80"
-          alt='brnd-logo'
-          />
+  return (
+    <Navbar
+      className=""
+      sticky="top"
+      bg="dark"
+      data-bs-theme="dark"
+      expand="lg"
+    >
+      <div className="ps-1">
+        <Navbar.Brand href="/" classsName="nav-logo">
+          <img src={Logo} width="80" height="80" alt="brnd-logo" />
         </Navbar.Brand>
-        </div>
-          <Navbar.Toggle className='mx-3' aria-controls="responsive-navbar-nav"/>
-          <Navbar.Collapse>
-        <Container className='bg-dark'>
+      </div>
+      <Navbar.Toggle
+        className="nav-menu me-1"
+        aria-controls="responsive-navbar-nav"
+      />
+      <Navbar.Collapse className="nav-menu">
+        <Container className="bg-dark">
           <Nav className="nav-links py-2 text-center">
-            <Nav.Link href="/" activeStyle>Home</Nav.Link>
-            <Nav.Link href="/portfolio">Projects
-            </Nav.Link>            
+            <Nav.Link href="/" activeStyle>
+              Home
+            </Nav.Link>
+            <Nav.Link href="/portfolio">Projects</Nav.Link>
             <Nav.Link href="/#about">About me</Nav.Link>
             <Nav.Link href="/#services">Services</Nav.Link>
             <Nav.Link href="/#contact">Contact</Nav.Link>
             <Nav.Link href="/#reviews">Reviews</Nav.Link>
           </Nav>
-      </Container>
-        <div className='text-center px-3'>
-            <Button href='/ResumePdf' variant='outline-secondary' className=''>Resume</Button>
+        </Container>
+        <div className="text-center px-3">
+          <Button href="/ResumePdf" variant="outline-secondary" className="">
+            Resume
+          </Button>
         </div>
-          </Navbar.Collapse>       
-        </Navbar>
-    )
-}
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
 
 export default Header;
